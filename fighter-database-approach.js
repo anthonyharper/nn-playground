2591358562aaee6b3c36fb876f9975150f6aa980
Anthony Harper <anthony.harper@gmail.com>
2018-04-28 17:54:14 -0400

    Groundwork for a database approach.

---
+++ fighter-database-approach.js
@@ -0,0 +1,34 @@
+var sql = require ('sqlite3').verbose();
+let db = new sql.Database('./db/fighters.db', (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the fighters SQlite database.');
+});
+
+var synaptic = require('synaptic');
+
+// create the network
+const { Layer, Network } = synaptic;
+
+// need to populate the fighter database with relevant stats about the most recently resolved event, which will need full fight history of each fighter, including each of their past opponents, which rapidly spirals into a full database of every MMA event / fighter... Sherdog already has this so it's probably time to write that scraper
+
+// then train the network to accurately predict the outcomes of the last event, making sure that the outcomes of that event are scrubbed from the training data set.
+
+// then persist the net like so:
+/*
+    // Export the network to a JSON which you can save as plain text
+    var exported = myNetwork.toJSON();
+
+    // Convert the network back to useable network
+    var imported = Network.fromJSON(exported);
+*/
+
+// then set it loose on the next event
+
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Closed the database connection.');
+});
