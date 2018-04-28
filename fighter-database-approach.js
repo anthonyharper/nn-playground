e3d77fdf77bb71568e5aa7358fc3aec29690d390
Anthony Harper <anthony.harper@gmail.com>
2018-04-28 18:01:11 -0400

    Input values should be between 0 and 1.

--- fighter-database-approach.js
+++ fighter-database-approach.js
@@ -13,6 +13,7 @@ const { Layer, Network } = synaptic;
 
 // need to populate the fighter database with relevant stats about the most recently resolved event, which will need full fight history of each fighter, including each of their past opponents, which rapidly spirals into a full database of every MMA event / fighter... Sherdog already has this so it's probably time to write that scraper
 
+// NOTE TO SELF: Input values should be between 0 and 1, normalize your inputs
 // then train the network to accurately predict the outcomes of the last event, making sure that the outcomes of that event are scrubbed from the training data set.
 
 // then persist the net like so:
